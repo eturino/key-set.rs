dcf5ffbb3c70754479c1f768d65d0105cf1957d5
Eduardo Turiño <eturino@eturino.com>
2020-12-06 04:32:36 +0000

    feat: added .invert()

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,20 @@ where
     AllExceptSome(Vec<T>),
 }
 
+impl<T> KeySet<T>
+where
+    T: Ord + Debug + Clone,
+{
+    fn invert(&self) -> Self {
+        match self {
+            KeySet::All => KeySet::None,
+            KeySet::None => KeySet::All,
+            KeySet::Some(e) => key_set_all_except_some(e),
+            KeySet::AllExceptSome(e) => key_set_some(e),
+        }
+    }
+}
+
 impl<T> std::fmt::Display for KeySet<T>
 where
     T: Ord + Debug + Clone,
@@ -121,6 +135,19 @@ mod tests {
         assert_eq!(ks_all.clone(), KeySet::All);
         assert_eq!(ks_some.clone(), KeySet::Some(vec![1, 2, 3]));
         assert_eq!(ks_aes.clone(), KeySet::AllExceptSome(vec![1, 2, 3]));
+    }
+
+    #[test]
+    fn test_invert() {
+        let ks_none: KeySet<i32> = KeySet::None;
+        let ks_all: KeySet<i32> = KeySet::All;
+        let ks_some: KeySet<i32> = KeySet::Some(vec![1, 2, 3]);
+        let ks_aes: KeySet<i32> = KeySet::AllExceptSome(vec![1, 2, 3]);
+
+        assert_eq!(ks_all.invert(), KeySet::None);
+        assert_eq!(ks_none.invert(), KeySet::All);
+        assert_eq!(ks_aes.invert(), KeySet::Some(vec![1, 2, 3]));
+        assert_eq!(ks_some.invert(), KeySet::AllExceptSome(vec![1, 2, 3]));
     }
 
     #[test]
